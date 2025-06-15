095ba75c5e33bbb2a0dcb0af2e9e0b5ba18d1309
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-15 19:08:25 +0100

    Catch routes for undefined endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { Request, Response, NextFunction } from 'express';
 
 import postsRouter from './routes/post.routes';
+import { NotFoundError } from './utils/errors';
 import errorHandler from './middlewares/errorHandler';
 
 dotenv.config();
@@ -12,6 +14,14 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use('/api/posts', postsRouter);
+
+// Catch-all route for undefined endpoints
+app.use((
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+  next(new NotFoundError(`Route ${req.originalUrl} not found`));
+});
 
 // Error Handler
 app.use(errorHandler);
