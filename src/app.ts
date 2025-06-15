ef68cbbc5794e80d4dafd4841382de28dcc1cb37
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-15 13:07:45 +0100

    Use error handler middleware & App Logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+// Error Handler
+app.use(errorHandler);
 
 export default app;
