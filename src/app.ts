2bd1a84ecf67dccbf312bc8e64f9143380ea5eb7
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-15 14:15:08 +0100

    Use mapped post routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
+
+import postsRouter from './routes/post.routes';
 import errorHandler from './middlewares/errorHandler';
+
 
 const app = express();
 app.use(express.json());
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use('/api/posts', postsRouter);
 
 // Error Handler
 app.use(errorHandler);
